a56064f35ccd093ed8ae3a8cc4d03f51bfeb33ed
wss-git <wssgryx@163.com>
2021-07-29 09:40:29 +0800

    feat: support custom fc-endpoint when create fc client

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,14 +1,24 @@
 import FC from '@alicloud/fc2';
+import * as core from '@serverless-devs/core';
 import { ICredentials } from '../interface/entity';
 
 export default class Client {
-  static buildFcClient(region: string, credentials: ICredentials) {
+  static async buildFcClient(region: string, credentials: ICredentials) {
     return new FC(credentials.AccountID, {
       accessKeyID: credentials.AccessKeyID,
       accessKeySecret: credentials.AccessKeySecret,
       securityToken: credentials.SecurityToken,
       region,
+      endpoint: await this.getFcEndpoint(),
       timeout: 6000000,
     })
+  }
+
+  private static async getFcEndpoint(): Promise<string | undefined> {
+    const fcDefault = await core.loadComponent('devsapp/fc-default');
+    const fcEndpoint: string = await fcDefault.get({ args: 'fc-endpoint' });
+    if (!fcEndpoint) { return undefined; }
+    const enableFcEndpoint: any = await fcDefault.get({ args: 'enable-fc-endpoint' });
+    return (enableFcEndpoint === true || enableFcEndpoint === 'true') ? fcEndpoint : undefined;
   }
 }
