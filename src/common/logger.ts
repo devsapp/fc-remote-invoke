78ab2a5b60639bcabffdc186601fc13cc092854d
千风 <qianfeng.zqf@alibaba-inc.com>
2021-08-09 17:07:15 +0800

    build: adapt to ncc build and remove unnecessary files

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,5 +1,4 @@
 
-import i18n from './i18n';
 import { Logger } from '@serverless-devs/core';
 
 export default class ComponentLogger {
@@ -8,26 +7,26 @@ export default class ComponentLogger {
     ComponentLogger.CONTENT = content;
   }
   static log(m, color?: 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'whiteBright' | 'gray') {
-    Logger.log(i18n.__(m) || m, color);
+    Logger.log(m, color);
   }
   static info(m) {
-    Logger.info(ComponentLogger.CONTENT, i18n.__(m) || m);
+    Logger.info(ComponentLogger.CONTENT, m);
   }
 
   static debug(m) {
-    Logger.debug(ComponentLogger.CONTENT, i18n.__(m) || m);
+    Logger.debug(ComponentLogger.CONTENT, m);
   }
 
   static error(m) {
-    Logger.error(ComponentLogger.CONTENT, i18n.__(m) || m);
+    Logger.error(ComponentLogger.CONTENT, m);
   }
 
   static warning(m) {
-    Logger.warn(ComponentLogger.CONTENT, i18n.__(m) || m);
+    Logger.warn(ComponentLogger.CONTENT, m);
   }
 
   static success(m) {
-    Logger.log(i18n.__(m) || m, 'green');
+    Logger.log(m, 'green');
   }
 }
 
