3f41b8c50aae070262469a6bc79ccbead2a1d067
wss-git <wssgryx@163.com>
2021-10-18 16:47:31 +0800

    build: 0.0.18

--- lib/lib/client.d.ts
+++
@@ -1,5 +0,0 @@
-import { ICredentials } from '../interface/entity';
-export default class Client {
-    static buildFcClient(region: string, credentials: ICredentials): Promise<any>;
-    private static getFcEndpoint;
-}
