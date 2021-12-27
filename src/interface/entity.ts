853611b69f51faa37eed11cff7b742c65de02fc7
wss-git <wssgryx@163.com>
2021-12-27 18:09:00 +0800

    fix: invoke function add timeout

--- src/interface/entity.ts
+++ src/interface/entity.ts
@@ -25,8 +25,10 @@ export interface IProperties {
   region: string;
   serviceName: string;
   functionName: string;
+  runtime?: string;
   qualifier?: string;
   domainName?: string;
+  timeout?: string;
 }
 export function isProperties(args: any): args is IProperties {
   if (!args) {
